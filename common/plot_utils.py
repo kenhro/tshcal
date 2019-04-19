16433bcdeefe9b73cccd8a0f7e935a944ff95078
pims <pims@mimsy.tsc.grc.nasa.gov>
2019-04-19 18:48:29 +0000

    Getting full complement of TshesAccelPacket now that I fully implemented stream-based protocol of TCP [not a message-based protocol].

--- common/plot_utils.py
+++ common/plot_utils.py
@@ -8,7 +8,7 @@ import matplotlib.animation as animation
 from matplotlib.widgets import TextBox
 
 
-PKT_SIZE = 9  # 512 is typical for TSH -- right?
+PKT_SIZE = 9  # 256 or 512 is typical for TSH -- right?
 DISPLAY_PTS = PKT_SIZE * 5  # FIXME display width (num pts) for WHAT length of time [use rate to reckon pts]
 
 
@@ -42,7 +42,54 @@ class RealtimePlot(object):
         animation.FuncAnimation(figure, wrapper, interval=interval)
 
 
-def main():
+class TshRealtimePlot(RealtimePlot):
+
+    def __init__(self, axes, max_pts=DISPLAY_PTS):
+        super().__init__(axes, max_pts=DISPLAY_PTS)
+
+    def submit_ymin(self, text): self.submit_ylim('bottom', text)
+
+    def submit_ymax(self, text): self.submit_ylim('top', text)
+
+    def submit_ylim(self, key, txt):
+        # TODO gracefully reject bogus values (no change, just warn & restore previous state)
+        val = float(txt)
+        kwargs = {key: val}
+        self.axes.set_ylim(**kwargs)  # define function within function so we have this axes within scope
+        plt.draw()
+
+    def run(self):
+
+        # establish ymin textbox
+        txtbox_ymin = plt.axes([0.05, 0.06, 0.04, 0.045])
+        initial_text = "-1"
+        ymin = float(initial_text)
+        axes.set_ylim(bottom=float(ymin))
+        text_box_ymin = TextBox(txtbox_ymin, 'ymin', initial=initial_text)
+        text_box_ymin.on_submit(submit_ymin)
+
+        # establish ymax textbox
+        txtbox_ymax = plt.axes([0.05, 0.9, 0.04, 0.045])
+        initial_text = "10"
+        ymax = float(initial_text)
+        axes.set_ylim(top=float(ymax))
+        text_box_ymax = TextBox(txtbox_ymax, 'ymax', initial=initial_text)
+        text_box_ymax.on_submit(submit_ymax)
+
+        # other parameters related to plotting
+        sleep_sec = 1
+        base_time = datetime.datetime(1970, 1, 1)
+        delta_sec = 1
+
+        while True:
+            t_values = np.array([base_time + datetime.timedelta(seconds=i) for i in range(PKT_SIZE)])
+            y_values = np.linspace(1.0, PKT_SIZE, num=PKT_SIZE) + np.random.standard_normal(PKT_SIZE) / 3.0
+            display.add(t_values, y_values)
+            base_time = t_values[-1] + datetime.timedelta(seconds=delta_sec)
+            plt.pause(sleep_sec)
+
+
+def OLDmain():
 
     def submit_ymin(text): submit_ylim('bottom', text)
 
@@ -58,7 +105,44 @@ def main():
     # initialize figure
     fig, axes = plt.subplots()
     plt.subplots_adjust(bottom=0.2)
-    display = RealtimePlot(axes)
+    display = TshRealtimePlot(axes)
+    mng = plt.get_current_fig_manager()
+    mng.resize(1600, 900)  # width, height in pixels
+
+    # establish ylim textboxes
+    txtbox_ymin = plt.axes([0.05, 0.06, 0.04, 0.045])
+    initial_text = "-1"
+    ymin = float(initial_text)
+    axes.set_ylim(bottom=float(ymin))
+    text_box_ymin = TextBox(txtbox_ymin, 'ymin', initial=initial_text)
+    text_box_ymin.on_submit(submit_ymin)
+
+    txtbox_ymax = plt.axes([0.05, 0.9, 0.04, 0.045])
+    initial_text = "10"
+    ymax = float(initial_text)
+    axes.set_ylim(top=float(ymax))
+    text_box_ymax = TextBox(txtbox_ymax, 'ymax', initial=initial_text)
+    text_box_ymax.on_submit(submit_ymax)
+
+    # other parameters related to plotting
+    sleep_sec = 1
+    base_time = datetime.datetime(1970, 1, 1)
+    delta_sec = 1
+
+    while True:
+        t_values = np.array([base_time + datetime.timedelta(seconds=i) for i in range(PKT_SIZE)])
+        y_values = np.linspace(1.0, PKT_SIZE, num=PKT_SIZE) + np.random.standard_normal(PKT_SIZE) / 3.0
+        display.add(t_values, y_values)
+        base_time = t_values[-1] + datetime.timedelta(seconds=delta_sec)
+        plt.pause(sleep_sec)
+
+
+def main():
+
+    # initialize figure
+    fig, axes = plt.subplots()
+    plt.subplots_adjust(bottom=0.2)
+    display = TshRealtimePlot(axes)
     mng = plt.get_current_fig_manager()
     mng.resize(1600, 900)  # width, height in pixels
 
@@ -91,4 +175,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    OLDmain()
--- tests/socket_test.py
+++ tests/socket_test.py
@@ -5,6 +5,7 @@ import json
 import struct
 import socket
 import datetime
+import numpy as np
 
 from tshcal.secret import TSHES13_IPADDR
 from tshcal.common.tshes_params_packet import TshesMessage
@@ -28,176 +29,200 @@ def eric_example(ip_addr, port=9750):
     s.close()
 
 
+def divvy_up(num, divisor=16):
+    """return tuple with 2 values: floor(num/divisor) and num%divisor
+    :param num: integer number of values
+    :param divisor: integer divisor, e.g. number of bytes in each record (default is 16)
+    :return: a tuple with 2 values: floor(num/divisor) and num%divisor
+    """
+    return int(np.floor(num/divisor)), num % divisor
+
+
+def recvall(sock, n):
+    """helper function to receive n bytes from (sock)et or return None if EOF is hit"""
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
 def raw_data_from_socket(ip_addr, port=9750):
-    """establish socket connection to tsh on data port (9750) and show pertinent data for confidence in this code"""
+    """establish socket connection to [tsh] (ip_addr)ess on data port (9750) and show pertinent data"""
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip_addr, port))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip_addr, port))
+        while True:
+            data = s.recv(8192)  # FIXME power of 2 is recommended, but not sure what optimum value to use here
+            if data:
+                if len(data) >= 16:  # FIXME Why was this value 80 in Ted's code [maybe it was MySQL db goodness?]
 
-    while True:
-        data = s.recv(1024)
-        if data:
-            if len(data) >= 80:
+                    # get selector value
+                    byte2 = struct.unpack('c', bytes([data[40]]))[0]
+                    byte3 = struct.unpack('c', bytes([data[41]]))[0]
+                    selector = ord(byte2) * 256 + ord(byte3)
 
-                # examine bytes, just enough (similar to "peek" Eric noted) to be sure we are onto a TshesAccelPacket
+                    # make sure we have selector that corresponds to a TshesAccelPacket; either real-time or replay
+                    accel_pkt = (selector == 170) or (selector == 171)
+                    if accel_pkt:
 
-                # get selector value
-                byte2 = struct.unpack('c', bytes([data[40]]))[0]
-                byte3 = struct.unpack('c', bytes([data[41]]))[0]
-                selector = ord(byte2) * 256 + ord(byte3)
+                        # examine structure before "Data" payload (TshesAccelPacket), starts @ byte 44 of tshes message
+                        tm = TshesMessage(data)
+                        # tm.enum_bytes()
 
-                # make sure we have selector that corresponds to a TshesAccelPacket; either real-time or replay
-                accel_pkt = (selector == 170) or (selector == 171)  # || (selector == 177)
-                if accel_pkt:
+                        # --- NOW HERE WE TRANSITION TO TshesAccelPacket ---
 
-                    # examine header/wrapper around "Data", TshesAccelPacket, which starts @ byte 44 of "tshes message"
-                    tm = TshesMessage(data)
-                    # tm.enum_bytes()
+                        # tsh identifier
+                        tshes_id = data[44:60]
+                        tshes_id = tshes_id.replace(b'-', b'').replace(b'\0', b'')  # delete dashes and nulls
+                        tshes_id = tshes_id[-4:]  # keep last 4 characters only, i.e., "es13"
 
-                    # --- HERE WE TRANSITION TO TshesAccelPacket INFO ---
+                        # counter
+                        counter = struct.unpack('!I', data[60:64])[0]  # Network byte order
 
-                    # tsh identifier
-                    tshes_id = data[44:60]
-                    tshes_id = tshes_id.replace(b'-', b'').replace(b'\0', b'')  # delete dashes and nulls
-                    tshes_id = tshes_id[-4:]  # keep last 4 characters only, i.e., "es13"
+                        # timestamp
+                        sec, usec = struct.unpack('!II', data[64:72])  # Network byte order
+                        timestamp = sec + usec / 1000000.0
 
-                    # counter
-                    counter = struct.unpack('!I', data[60:64])[0]  # Network byte order
+                        # packet_status
+                        packet_status = struct.unpack('!i', data[72:76])[0]  # Network byte order
 
-                    # timestamp
-                    sec, usec = struct.unpack('!II', data[64:72])  # Network byte order
-                    timestamp = sec + usec / 1000000.0
+                        # number of samples
+                        num_samples = struct.unpack('!i', data[76:80])[0]  # Network byte order
 
-                    # packet_status
-                    packet_status = struct.unpack('!i', data[72:76])[0]  # Network byte order
+                        # get rate and cutoff_freq from packet status
+                        rate_bits = (packet_status & 0x0f00) >> 8
+                        if (rate_bits == 0):
+                            rate, cutoff_freq = 7.8125, 3.2
+                        elif (rate_bits == 1):
+                            rate, cutoff_freq = 15.625, 6.3
+                        elif (rate_bits == 2):
+                            rate, cutoff_freq = 31.25, 12.7
+                        elif (rate_bits == 3):
+                            rate, cutoff_freq = 62.5, 25.3
+                        elif (rate_bits == 4):
+                            rate, cutoff_freq = 125.0, 50.6
+                        elif (rate_bits == 5):
+                            rate, cutoff_freq = 250.0, 101.4
+                        elif (rate_bits == 6):
+                            rate, cutoff_freq = 500.0, 204.2
+                        elif (rate_bits == 7):
+                            rate, cutoff_freq = 1000.0, 408.5
+                        elif (rate_bits == 8):
+                            rate, cutoff_freq = 125.0, 23.5
+                        else:
+                            # FIXME how do we gracefully proceed with wrong rate info?
+                            rate, cutoff_freq = 0.0, 0.0
 
-                    # number of samples
-                    num_samples = struct.unpack('!i', data[76:80])[0]  # Network byte order
+                        # get gain and input from packet status
+                        gain_bits = packet_status & 0x001f
+                        if (gain_bits == 0):
+                            gain, input = 1.0, 'Ground'  # _input_ is not used as far as I can tell
+                        elif (gain_bits == 1):
+                            gain, input = 2.5, 'Ground'
+                        elif (gain_bits == 2):
+                            gain, input = 8.5, 'Ground'
+                        elif (gain_bits == 3):
+                            gain, input = 34.0, 'Ground'
+                        elif (gain_bits == 4):
+                            gain, input = 128.0, 'Ground'
+                        elif (gain_bits == 8):
+                            gain, input = 1.0, 'Test'
+                        elif (gain_bits == 9):
+                            gain, input = 2.5, 'Test'
+                        elif (gain_bits == 10):
+                            gain, input = 8.5, 'Test'
+                        elif (gain_bits == 11):
+                            gain, input = 34.0, 'Test'
+                        elif (gain_bits == 12):
+                            gain, input = 128.0, 'Test'
+                        elif (gain_bits == 16):
+                            gain, input = 1.0, 'Signal'
+                        elif (gain_bits == 17):
+                            gain, input = 2.5, 'Signal'
+                        elif (gain_bits == 18):
+                            gain, input = 8.5, 'Signal'
+                        elif (gain_bits == 19):
+                            gain, input = 34.0, 'Signal'
+                        elif (gain_bits == 20):
+                            gain, input = 128.0, 'Signal'
+                        elif (gain_bits == 24):
+                            gain, input = 1.0, 'Vref'
+                        elif (gain_bits == 25):
+                            gain, input = 1.0, 'Sensor test'
+                        elif (gain_bits == 26):
+                            gain, input = 2.0, 'Sensor test'
+                        else:
+                            # FIXME how do we gracefully proceed with wrong gain info?
+                            gain, input = 0.0, 'Unknown'
 
-                    # get rate and cutoff_freq from packet status
-                    rate_bits = (packet_status & 0x0f00) >> 8
-                    if (rate_bits == 0):
-                        rate, cutoff_freq = 7.8125, 3.2
-                    elif (rate_bits == 1):
-                        rate, cutoff_freq = 15.625, 6.3
-                    elif (rate_bits == 2):
-                        rate, cutoff_freq = 31.25, 12.7
-                    elif (rate_bits == 3):
-                        rate, cutoff_freq = 62.5, 25.3
-                    elif (rate_bits == 4):
-                        rate, cutoff_freq = 125.0, 50.6
-                    elif (rate_bits == 5):
-                        rate, cutoff_freq = 250.0, 101.4
-                    elif (rate_bits == 6):
-                        rate, cutoff_freq = 500.0, 204.2
-                    elif (rate_bits == 7):
-                        rate, cutoff_freq = 1000.0, 408.5
-                    elif (rate_bits == 8):
-                        rate, cutoff_freq = 125.0, 23.5
-                    else:
-                        # FIXME how do we gracefully proceed with wrong rate info?
-                        rate, cutoff_freq = 0.0, 0.0
+                        # get unit from packet status
+                        unit_bits = (packet_status & 0x0060) >> 5
+                        if (unit_bits == 0):
+                            unit = 'counts'
+                        elif (unit_bits == 1):
+                            unit = 'volts'
+                        elif (unit_bits == 2):
+                            unit = 'g'
+                        else:
+                            # FIXME how do we gracefully proceed with wrong units info?
+                            unit = 'g'
 
-                    # get gain and input from packet status
-                    gain_bits = packet_status & 0x001f
-                    if (gain_bits == 0):
-                        gain, input = 1.0, 'Ground'  # _input_ is not used as far as I can tell
-                    elif (gain_bits == 1):
-                        gain, input = 2.5, 'Ground'
-                    elif (gain_bits == 2):
-                        gain, input = 8.5, 'Ground'
-                    elif (gain_bits == 3):
-                        gain, input = 34.0, 'Ground'
-                    elif (gain_bits == 4):
-                        gain, input = 128.0, 'Ground'
-                    elif (gain_bits == 8):
-                        gain, input = 1.0, 'Test'
-                    elif (gain_bits == 9):
-                        gain, input = 2.5, 'Test'
-                    elif (gain_bits == 10):
-                        gain, input = 8.5, 'Test'
-                    elif (gain_bits == 11):
-                        gain, input = 34.0, 'Test'
-                    elif (gain_bits == 12):
-                        gain, input = 128.0, 'Test'
-                    elif (gain_bits == 16):
-                        gain, input = 1.0, 'Signal'
-                    elif (gain_bits == 17):
-                        gain, input = 2.5, 'Signal'
-                    elif (gain_bits == 18):
-                        gain, input = 8.5, 'Signal'
-                    elif (gain_bits == 19):
-                        gain, input = 34.0, 'Signal'
-                    elif (gain_bits == 20):
-                        gain, input = 128.0, 'Signal'
-                    elif (gain_bits == 24):
-                        gain, input = 1.0, 'Vref'
-                    elif (gain_bits == 25):
-                        gain, input = 1.0, 'Sensor test'
-                    elif (gain_bits == 26):
-                        gain, input = 2.0, 'Sensor test'
-                    else:
-                        # FIXME how do we gracefully proceed with wrong gain info?
-                        gain, input = 0.0, 'Unknown'
+                        # get adjustment from packet status
+                        adj_bits = (packet_status & 0x0080) >> 7
+                        adjustment = 'no-compensation'
+                        if adj_bits == 1:
+                            adjustment = 'temperature-compensation'
 
-                    # get unit from packet status
-                    unit_bits = (packet_status & 0x0060) >> 5
-                    if (unit_bits == 0):
-                        unit = 'counts'
-                    elif (unit_bits == 1):
-                        unit = 'volts'
-                    elif (unit_bits == 2):
-                        unit = 'g'
-                    else:
-                        # FIXME how do we gracefully proceed with wrong units info?
-                        unit = 'g'
+                        # compute end time from start, number of samples and rate
+                        end_time = timestamp + (num_samples - 1) / rate
 
-                    # get adjustment from packet status
-                    adj_bits = (packet_status & 0x0080) >> 7
-                    adjustment = 'no-compensation'
-                    if adj_bits == 1:
-                        adjustment = 'temperature-compensation'
+                        # build array of accel data
+                        xyz = []
 
-                    # compute end time from start, number of samples and rate
-                    end_time = timestamp + (num_samples - 1) / rate
+                        # compute delta samples missing from current volley of bytes; 1 sample = 16 bytes (x,y,z,dio)
+                        received_samples, left_over_bytes = divvy_up(len(data[80:]))
+                        deficit_samples = num_samples - received_samples
+                        deficit_bytes = deficit_samples * 16 - left_over_bytes
 
-                    # get array of accel data
-                    xyz = []
-                    kludge_num_samples = int(len(data[80:]) / 16)
-                    # print(num_samples, kludge_num_samples)
-                    for i in range(kludge_num_samples):  # range(num_samples):
-                        start = 80 + 16 * i
-                        stop = start + 16
-                        # print(count, start, stop)
-                        # x, y, z, digitalIOstatus = struct.unpack('!fffI', data[start:stop])  # Network byte order
-                        # we are totally ignoring digital io status
-                        # self.handleDigitalIOstatus(digitalIOstatus, i)
-                        # if convert:
-                        #     x, y, z = x * mx + bx, y * my + by, z * mz + bz
-                        x, y, z, junk = struct.unpack('!fffI', data[start:stop])  # Network byte order
-                        xyz.append((x, y, z))
+                        # append the samples we have received so far
+                        for i in range(received_samples):
+                            start = 80 + 16 * i
+                            stop = start + 16
+                            x, y, z, dio = struct.unpack('!fffI', data[start:stop])  # Network byte order
+                            # we are ignoring digital io status (dio)
+                            # self.handleDigitalIOstatus(digitalIOstatus, i)
+                            # if convert:
+                            #     x, y, z = x * mx + bx, y * my + by, z * mz + bz
+                            xyz.append((x, y, z))
 
-                    print('tm:[', str(tm).replace('\n', ' '), ']', tshes_id, counter, unix_to_human_time(timestamp),
-                          packet_status, num_samples, rate, cutoff_freq, gain, input, unit, adjustment,
-                          unix_to_human_time(end_time))
+                        # keep remainder bytes and prepend to balance of samples called for in TshesAccelPacket struct
+                        more_data = data[stop:]
+                        more_data += recvall(s, deficit_bytes)  # remainder of bytes we need to get num_samples filled
 
-                    if False:
-                        c  = 0; print('%3d' % c, xyz[c])
-                        c += 1; print('%3d' % c, xyz[c])
-                        c += 1; print('%3d' % c, xyz[c])
-                        print('  :')
-                        c = kludge_num_samples - 3; print('%3d' % c, xyz[c])
-                        c += 1; print('%3d' % c, xyz[c])
-                        c += 1; print('%3d' % c, xyz[c])
+                        # append deficit samples to get num_samples in total
+                        for i in range(deficit_samples):
+                            start = 16 * i
+                            stop = start + 16
+                            x, y, z, dio = struct.unpack('!fffI', more_data[start:stop])  # Network byte order
+                            xyz.append((x, y, z))
 
-                    # pkt = SamsTshEs(data)
-                    #
-                    # print(pkt)
+                        print('len(data) = %d' % len(data), 'tm:[', str(tm).replace('\n', ' '), ']', tshes_id, counter,
+                              unix_to_human_time(timestamp), packet_status, num_samples, rate, cutoff_freq, gain,
+                              input, unit, adjustment, unix_to_human_time(end_time), received_samples, left_over_bytes,
+                              deficit_samples, stop)
 
-        else:
-            break
+                        # print('%3d' % 0, xyz[0])
+                        # print('  :')
+                        # print('%3d' % len(xyz), xyz[-1])
 
-    s.close()
+                else:
+                    print('unhandled branch with len(data) = %d' % len(data))
+
+            else:
+                break
 
 
 def ken_json_echo_client_example(ip_addr, port, json_data):
