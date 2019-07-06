3616f7ff96d3e7ec89146b0fe0ca5daf4e682ff3
Ken <none>
2019-07-06 09:41:05 -0400

    Update fifo.py
    Probably should have a TSH (or TshData) class.

--- common/fifo.py
+++ common/fifo.py
@@ -7,11 +7,14 @@ from tshcal.defaults import TSH_BUFFER_SEC
 from tshcal.commanding.tsh_commands import fake_query_tsh_sample_rate
 
 
+# FIXME should below few lines be creation (or pass-in) of TSH object? -- something we have not gotten to yet
 tsh_id = 'tshes-44'
+tsh_ax = 'x'
 fs = fake_query_tsh_sample_rate(tsh_id)
-buff_len = np.int(np.ceil(fs * TSH_BUFFER_SEC))
+tsh_buffer_len = np.int(np.ceil(fs * TSH_BUFFER_SEC))
+# FIXME should above few lines be creation (or pass-in) of TSH object? -- something we have not gotten to yet
 
-d = deque(maxlen=buff_len)
+d = deque(maxlen=tsh_buffer_len)
 
 for i in range(6):
     d.appendleft(i)
