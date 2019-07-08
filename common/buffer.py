042672af0c64f9017a22bab2a77d9d3c5fcaf700
Ken <none>
2019-07-08 19:36:01 -0400

    Rename fifo to buffer since buffer fills, but does not empty

--- common/buffer.py
+++ common/buffer.py
@@ -17,15 +17,15 @@ class Tsh(object):
         return s
 
 
-class TshAccelFifo(object):
+class TshAccelBuffer(object):
 
     def __init__(self, tsh, num):
         self.tsh = tsh  # tsh object -- to set/get some operating parameters
-        self.num = num  # size of FIFO buffer (i.e. number of points)
+        self.num = num  # size of data buffer (i.e. number of points)
         # TODO next 2 lines will fill very fast, but be careful...np.empty is garbage values
         self.xyz = np.empty((num, 3))  # NOTE: this will contain garbage values
         self.xyz.fill(np.nan)          # NOTE: this cleans up garbage values, replacing with NaNs
-        self.is_full = False           # flag that goes True when FIFO is full
+        self.is_full = False           # flag that goes True when data buffer is full
         self.idx = 0
 
     def __str__(self):
@@ -35,16 +35,16 @@ class TshAccelFifo(object):
         return s
 
     def write_spreadsheet(self, fname):
-        print('writing spreadsheet data from %s fifo to %s' % (self.tsh.name, fname))
+        print('writing spreadsheet data from %s buffer to %s' % (self.tsh.name, fname))
 
     def write_raw(self, fname):
-        print('writing raw data from %s fifo to %s' % (self.tsh.name, fname))
+        print('writing raw data from %s buffer to %s' % (self.tsh.name, fname))
 
     def add(self, more):
 
         if self.is_full:
-            # TODO log entry that we tried to add to a fifo that's already full
-            print('fifo already full')
+            # TODO log entry that we tried to add to a buffer that's already full
+            print('buffer already full')
             return
 
         offset = more.shape[0]
@@ -52,38 +52,38 @@ class TshAccelFifo(object):
             offset = self.xyz[self.idx:, :].shape[0]
             self.xyz[self.idx:self.idx + offset, :] = more[0:offset, :]
             self.is_full = True
-            # TODO add log entry that TshAccelFifo is now full (with how many rows)
+            # TODO add log entry that TshAccelBuffer is now full (with how many rows)
         else:
             self.xyz[self.idx:self.idx + offset, :] = more
         # print(self.idx, self.idx + offset)
         self.idx = self.idx + offset
 
 
-def demo_fifo():
+def demo_buffer():
 
-    # create FIFO buffer
+    # create data buffer
     num_rows = 9  # how many rows of x,y,z values
     tsh = Tsh('tshes-44', 250.1, 1.2)  # last 2 args put/gotten for convenience here
-    fifo = TshAccelFifo(tsh, num_rows)
+    buffer = TshAccelBuffer(tsh, num_rows)
 
     b = np.arange(6).reshape(2, 3)
-    fifo.add(b)
+    buffer.add(b)
 
     b = np.arange(9).reshape(3, 3)
-    fifo.add(b)
+    buffer.add(b)
 
     b = np.arange(30).reshape(10, 3)
-    fifo.add(b)
+    buffer.add(b)
 
     b = np.arange(60).reshape(20, 3)
-    fifo.add(b)
+    buffer.add(b)
 
-    print(fifo.xyz)
+    print(buffer.xyz)
 
-    fifo.write_spreadsheet('/tmp/somefile.xlsx')
-    fifo.write_raw('/tmp/filename.csv')
+    buffer.write_spreadsheet('/tmp/somefile.xlsx')
+    buffer.write_raw('/tmp/filename.csv')
 
 
 if __name__ == '__main__':
 
-    demo_fifo()
+    demo_buffer()
